6f40f02f3d7ab3c8a7f4a2362c6643c48e1a2322
iahc <15177799844@163.com>
2020-09-03 10:54:11 +0800

    update

--- update.sh
+++ update.sh
@@ -3,26 +3,32 @@ baseUrl="https://cdn.jsdelivr.net/gh/iahccc/PicBed/"
 
 status=`git status`
 
-#if [[ ! $status =~ "删除" ]]
-#then
-substr=${status##*以包含要提交的内容）}
-substr=${substr%提交*}
-substr=${substr%修改尚未加入提交*}
+if [[ $status =~ "以包含要提交的内容）" ]]
+then
+    substr=${status##*以包含要提交的内容）}
+    substr=${substr%提交*}
+    substr=${substr%修改尚未加入提交*}
+    
+    array=(`echo $substr`)
+    
+    result=''
+    for i in ${!array[@]}
+    do
+        result="${result}${baseUrl}${array[$i]}\n"
+    done 
+    
+    echo -e $result | xsel -i -b #将内容复制到粘贴板
+    
+    git pull origin
+    git add $basePath
+    git commit -m 'update'
+    git push origin
+    
+    echo -e $result
 
-array=(`echo $substr`)
-
-result=''
-for i in ${!array[@]}
-do
-    result="${result}${baseUrl}${array[$i]}\n"
-done 
-
-echo -e $result | xsel -i -b #将内容复制到粘贴板
-
-git pull origin
-git add $basePath
-git commit -m 'update'
-git push origin
-
-echo -e $result
-
+else
+    git pull origin
+    git add $basePath
+    git commit -m 'update'
+    git push origin
+fi
