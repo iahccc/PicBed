9631d8f8e23ea6f19974daacfdf92f905f57f2a2
iahc <15177799844@163.com>
2020-09-03 10:49:05 +0800

    update

--- update.sh
+++ update.sh
@@ -2,8 +2,12 @@ basePath="/home/iahc/图片/PicBed/"
 baseUrl="https://cdn.jsdelivr.net/gh/iahccc/PicBed/"
 
 status=`git status`
-substr=${status#*内容）}
+
+#if [[ ! $status =~ "删除" ]]
+#then
+substr=${status##*以包含要提交的内容）}
 substr=${substr%提交*}
+substr=${substr%修改尚未加入提交*}
 
 array=(`echo $substr`)
 
@@ -21,3 +25,4 @@ git commit -m 'update'
 git push origin
 
 echo -e $result
+
